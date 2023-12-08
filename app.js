db53b7c15e7416fc17b82f53c14246fd9adfdd4d
ananthu 7025 <ananthapadmanabhan7025@gmail.com>
2023-12-08 11:31:19 +0530

    Build file Updated

--- app.js
+++ app.js
@@ -5,16 +5,15 @@ const userRoutes = require("./routes/auth");
 const paymentRoutes = require('./routes/payment');
 const questionRoutes = require('./routes/question');
 const resultRoutes = require('./routes/result');
-// const fileUploadRoutes = require('./routes/fileUpload');
 const folderRouter = require('./routes/folder');
 const referralRoutes = require('./routes/referal');
 const userRoutesAdmin = require('./routes/admin');
-const CurrentAffaris = require('./routes/currentAffairs');
+const CurrentAffairs = require('./routes/currentAffairs');
 const folderFech = require('./routes/folderFetch');
 const studyPlan = require('./routes/studyPlan');
 const categoryRoutes = require('./routes/catogorey');
 const subcategoryRoutes = require('./routes/subcatogorey');
-
+const path = require('path');
 const errorHandler = require("./errorHandler");
 const cors = require("cors");
 const app = express();
@@ -30,10 +29,10 @@ mongoose.connect("mongodb://127.0.0.1:27017/PSC?directConnection=true", {
   .catch((err) => {
     console.error("Error connecting to the database:", err);
   });
-  
+
 app.use(bodyParser.json());
-app.use(errorHandler);
 app.use(cors());
+app.use(express.static(path.join(__dirname, 'public/build')));
 app.use("/api/user", userRoutes);
 app.use('/api/payment', paymentRoutes);
 app.use('/api', questionRoutes);
@@ -41,12 +40,15 @@ app.use('/api', resultRoutes);
 app.use('/api/folders', folderRouter);
 app.use('/api', referralRoutes);
 app.use('/api/admin/users', userRoutesAdmin);
-app.use('/api', CurrentAffaris);
+app.use('/api', CurrentAffairs);
 app.use('/api', folderFech);
 app.use('/api', studyPlan);
 app.use('/api', categoryRoutes);
 app.use('/api', subcategoryRoutes);
-
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'public/build', 'index.html'));
+});
+app.use(errorHandler);
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
