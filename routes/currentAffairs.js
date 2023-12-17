0065be75b6d626286a43c962200257742820c0d5
ananthu 7025 <ananthapadmanabhan7025@gmail.com>
2023-12-17 11:11:32 +0530

    Final Commit

--- routes/currentAffairs.js
+++ routes/currentAffairs.js
@@ -7,8 +7,13 @@ const client_id = credentials.web.client_id;
 const client_secret = credentials.web.client_secret;
 const redirect_uris = credentials.web.redirect_uris;
 const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
-
-const SCOPE = ['https://www.googleapis.com/auth/drive.metadata.readonly https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/drive.file']
+const SCOPE = [
+    // 'https://www.googleapis.com/auth/drive.metadata.readonly',
+    // 'https://www.googleapis.com/auth/userinfo.profile',
+    'https://www.googleapis.com/auth/drive.file'
+  ];
+  
+  
 
 router.get('/getAuthURL', (req, res) => {
     const authUrl = oAuth2Client.generateAuthUrl({
