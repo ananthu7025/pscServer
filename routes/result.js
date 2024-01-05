fc4199ab1cb056f7e8bbbce05505d0a707d160bb
ananthu 7025 <ananthapadmanabhan7025@gmail.com>
2024-01-05 16:57:56 +0530

    Pdfs updated

--- routes/result.js
+++ routes/result.js
@@ -5,5 +5,5 @@ const resultController = require('../controllers/result');
 router.post('/submit-quiz', resultController.submitQuiz);
 
 router.get('/user-results/:userID', resultController.getUserResults);
-
+router.get('/quiz-details/:resultID', resultController.getQuizDetails);
 module.exports = router;
